bd79fc51bab5a8be07e8af41159fa5b8bc23121e
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-09-23 20:23:41 +0700

    backend: add CreateTodo

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -1,12 +1,13 @@
 // import { TodosAccess } from './todosAcess';
 // import { AttachmentUtils } from './attachmentUtils';
 import { TodoItem } from '../models/TodoItem';
-// import { CreateTodoRequest } from '../requests/CreateTodoRequest';
+import { CreateTodoRequest } from '../requests/CreateTodoRequest';
 // import { UpdateTodoRequest } from '../requests/UpdateTodoRequest';
 // import { createLogger } from '../utils/logger';
-// import * as uuid from 'uuid';
+import * as uuid from 'uuid';
 // import * as createError from 'http-errors';
 import * as AWS from 'aws-sdk';
+import dateFormat from 'dateformat';
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
@@ -25,4 +26,22 @@ export async function getTodosForUser(userId: string): Promise<TodoItem[]> {
 
   const items = result.Items as TodoItem[];
   return items;
+}
+
+export async function createTodo(userId: string, newTodo: CreateTodoRequest): Promise<TodoItem> {
+  const itemId = uuid.v4()
+  const newItem: TodoItem = {
+    todoId: itemId,
+    userId: userId,
+    done: false,
+    createdAt: dateFormat(Date.now(), 'yyyy-mm-dd') as string,
+    ...newTodo,
+  }
+
+  await docClient.put({
+    TableName: todosTable,
+    Item: newItem,
+  }).promise();
+
+  return newItem;
 }
--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,23 +1,24 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import 'source-map-support/register'
-import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
-import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
-// import { getUserId } from '../utils';
-// import { createTodo } from '../../businessLogic/todos'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import 'source-map-support/register';
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest';
+import { getUserId } from '../utils';
+import { createTodo } from '../../businessLogic/todos';
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const userId = getUserId(event);
     const newTodo: CreateTodoRequest = JSON.parse(event.body);
-    // TODO: Implement creating a new TODO item
+    const newTodoItem = await createTodo(userId, newTodo);
 
     return {
-      statusCode: 200,
+      statusCode: 201,
       headers: {
         'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify({
-        newTodo
+        item: newTodoItem,
       }),
     }
   });
